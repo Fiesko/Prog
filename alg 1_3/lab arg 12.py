74e64c9ddadc67056d1beb1f36b0c4e0ce220f8c
MalevannyIlya <73445307+Fiesko@users.noreply.github.com>
2025-05-14 21:53:34 +0300

    Add files via upload

---
+++ alg 1_3/lab arg 12.py
@@ -0,0 +1,11 @@
+#Реализуйте рекурсивный алгоритм вычисления произведения элементов списка.
+def recursive_sum(x):
+    if not x:
+        return 0
+    return x[0] + recursive_sum(x[1:])
+
+numbers = [2, 3, 4, 5]
+result = recursive_sum(numbers)
+print(result)  
+
+# Сложность алгоритма: O(n^2)
